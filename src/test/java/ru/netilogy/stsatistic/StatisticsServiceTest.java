0f4ea74dd13175c5ea745a4cfa5fbf2ac4c7fd8d
iva1111 <mvg75@mail.ru>
2021-08-03 15:51:31 +0500

    update jacoco

--- src/test/java/ru/netilogy/stsatistic/StatisticsServiceTest.java
+++ src/test/java/ru/netilogy/stsatistic/StatisticsServiceTest.java
@@ -3,8 +3,6 @@ package ru.netilogy.stsatistic;
 import org.junit.jupiter.api.Test;
 import ru.netology.stsatistic.StatisticsService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class StatisticsServiceTest {
 
     @Test
@@ -12,7 +10,6 @@ class StatisticsServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
 
